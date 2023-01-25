de03db16289a1f24440de4b08cfba4c8106bce5e
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-25 11:54:42 +0300

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,182 @@
+"use strict";
+///308
+// setInterval(function(){console.log('meow')}, 3000)
+
+///309
+
+// let i = 100
+// setInterval(()=> console.log(i--), 1000)
+
+///310
+
+// let i = 10;
+
+// let timerId = setInterval(function() {
+// 	console.log(i--);
+	
+// 	if (i <= 0) {
+// 		clearInterval(timerId);
+// 	}
+// }, 1000);
+
+//311
+//1
+// let start = document.querySelector('#start');
+
+// start.addEventListener('click', function() {
+// 	let i = 100;
+	
+// 	setInterval(function() {
+// 		console.log(i--);
+// 	}, 1000);
+// });
+
+//2
+// let start = document.querySelector('#start');
+// start.addEventListener('click', function func() {
+// 	let i = 100;
+	
+// 	setInterval(function() {
+// 		console.log(i--);
+// 	}, 1000);
+//     this.removeEventListener('click', func);
+// });
+
+//312
+//1
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+// let timerId;
+
+// start.addEventListener('click', function() {
+// 	let i = 100;
+
+// 	timerId = setInterval(function() {
+//         console.log(i--)
+//         if (i <= 0) {
+//             clearInterval(timerId);
+//         }
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//2
+
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+// let timerId
+// start.addEventListener('click', function() {
+// 	    timerId = setInterval(function() {
+// 		let date = new Date;
+// 		console.log(date.getMinutes() + ' ' + date.getSeconds()
+// 			); 
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//3
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+
+// let timerId;
+
+// start.addEventListener('click', function() {
+// 	    timerId = setInterval(function() {
+// 		let date = new Date;
+// 		console.log(date.getMinutes() + ' ' + date.getSeconds()
+// 			); 
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//4
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+
+// let timerId;
+
+// start.addEventListener('click', function() {
+// 	timerId = setInterval(function() {
+// 		let date = new Date;
+// 		console.log(date.getMinutes() + ' ' + date.getSeconds()
+// 			); 
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//5
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+
+// let timerId;
+
+// start.addEventListener('click', function() {
+// 	    timerId = setInterval(function() {
+// 		let date = new Date;
+// 		console.log(date.getMinutes() + ' ' + date.getSeconds()
+// 			); 
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//6
+
+// let start = document.querySelector('#start');
+// let stop  = document.querySelector('#stop');
+
+// let timerId;
+
+// start.addEventListener('click', function func() {
+// 	    timerId = setInterval(function() {
+// 		let date = new Date;
+// 		console.log(date.getMinutes() + ' ' + date.getSeconds()
+// 			); 
+// 	}, 1000);
+//     this.removeEventListener('click', func);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+//Практика на таймеры и работу с DOM в JavaScript
+
+
+// let elem = document.querySelector('#elem');
+// setInterval(function() {
+// 	elem.value = Number(elem.value) + 1;
+// }, 1000);
+
+//1 
+//будет прибавлять как строки
+
+//2
+// let elem1 = document.querySelector('#elem1');
+// setInterval(function() {
+// 	elem1.value = Number(elem1.value) - 1;
+// }, 1000);
+
+//3
+// let elem1 = document.querySelector('#elem1');
+
+// let timerId = setInterval(function() {
+// 	elem1.value = Number(elem1.value) - 1;
+//     if(elem1.value <= 0){
+//         clearInterval(timerId)
+//     }
+// }, 1000);
